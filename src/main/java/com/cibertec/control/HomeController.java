f8a778cc28b6c97589078a6b289b4489903e50a4
i201823049 <i201823049@cibertec.edu.pe>
2022-10-25 16:17:42 -0500

    35% proyecto

--- src/main/java/com/cibertec/control/HomeController.java
+++ src/main/java/com/cibertec/control/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cibertec.model.Producto;
@@ -45,4 +46,11 @@ public class HomeController {
 		return"usuario/productohome";
 	}
 
+	@PostMapping("/cart")
+	public String addCart() {
+		return "usuario/carrito";
+	}
+	
+	
+	
 }
