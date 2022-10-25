fb96d08ff49c530c9bcbb7d60085687dc42f7ecd
i201823049 <i201823049@cibertec.edu.pe>
2022-10-25 08:43:22 -0500

    creacion de paquetes y clases principales

---
+++ src/main/java/com/cibertec/EscobarEcommerceApplication.java
@@ -0,0 +1,14 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class EscobarEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscobarEcommerceApplication.class, args);
+	}
+
+}
