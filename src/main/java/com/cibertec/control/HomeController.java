394de40f8763ab75e00fd6d9cbb84615bf92f096
i201823049 <i201823049@cibertec.edu.pe>
2022-10-25 16:57:29 -0500

    calcular precio carrito

--- src/main/java/com/cibertec/control/HomeController.java
+++ src/main/java/com/cibertec/control/HomeController.java
@@ -1,5 +1,7 @@
 package com.cibertec.control;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.cibertec.model.DetalleOrden;
+import com.cibertec.model.Orden;
 import com.cibertec.model.Producto;
 import com.cibertec.service.ProductoService;
 
@@ -23,6 +28,12 @@ public class HomeController {
 	
 	@Autowired
 	private ProductoService productoService;
+	
+	//Detalles recibo
+	List<DetalleOrden> detalles= new ArrayList<DetalleOrden>();
+	
+	//datos de orden
+	Orden orden = new Orden();
 	
 	
 	@GetMapping("")
@@ -47,7 +58,35 @@ public class HomeController {
 	}
 
 	@PostMapping("/cart")
-	public String addCart() {
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
+		DetalleOrden detalleOrden = new DetalleOrden();
+		Producto producto = new Producto();
+		double sumaTotal = 0;
+		
+		
+		
+		Optional<Producto>optionalProducto = productoService.get(id);
+		log.info("Producto añadido: {}", optionalProducto.get());
+		log.info("Cantidad: {}", cantidad);
+		
+		producto=optionalProducto.get();
+		
+		detalleOrden.setCantidad(cantidad);
+		detalleOrden.setPrecio(producto.getPrecio());
+		detalleOrden.setNombre(producto.getNombre());
+		detalleOrden.setTotal(producto.getPrecio()*cantidad);
+		detalleOrden.setProducto(producto);
+		
+		detalles.add(detalleOrden);		
+		
+		sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+		orden.setTotal(sumaTotal);
+		model.addAttribute("cart",detalles);
+		model.addAttribute("orden",orden);
+		
+		
+		
+		
 		return "usuario/carrito";
 	}
 	
