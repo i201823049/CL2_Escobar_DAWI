d709cc9ca150d570e13e36d9f2a5f81b9026a846
i201823049 <i201823049@cibertec.edu.pe>
2022-10-25 17:43:13 -0500

    ver carrito desde la cabecera

--- src/main/java/com/cibertec/control/HomeController.java
+++ src/main/java/com/cibertec/control/HomeController.java
@@ -74,7 +74,13 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio() * cantidad);
 		detalleOrden.setProducto(producto);
 
-		detalles.add(detalleOrden);
+		//validar que le producto no se añada 2 veces
+		Integer idProducto=producto.getId();
+		boolean ingresado=detalles.stream().anyMatch(p -> p.getProducto().getId()==idProducto);
+		
+		if (!ingresado) {
+			detalles.add(detalleOrden);
+		}
 
 		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 		orden.setTotal(sumaTotal);
@@ -107,4 +113,21 @@ public class HomeController {
 		return "usuario/carrito";
 
 	}
+	
+	@GetMapping("/getCart")
+	public String getCart(Model model) {
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+		//sesion
+		//model.addAttribute("sesion", session.getAttribute("idusuario"));
+		return "/usuario/carrito";
+	}
+	
+	
+	
+	
+	
+	
 }
