697a2caffc57299ae21082765dd49799c19aa4ca
i201823049 <i201823049@cibertec.edu.pe>
2022-10-27 09:08:11 -0500

    metodo para generar el numero de orden

--- src/main/java/com/cibertec/service/IOrdenService.java
+++ src/main/java/com/cibertec/service/IOrdenService.java
@@ -1,10 +1,18 @@
 package com.cibertec.service;
 
+import java.util.List;
+
 import com.cibertec.model.Orden;
 
 public interface IOrdenService {
 	
+	
+	List<Orden> findAll();
+	
+	
 	Orden save(Orden orden);
+	
+	
 	
 	
 
--- src/main/java/com/cibertec/service/OrdenServiceImpl.java
+++ src/main/java/com/cibertec/service/OrdenServiceImpl.java
@@ -1,5 +1,8 @@
 package com.cibertec.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,42 @@ public class OrdenServiceImpl implements IOrdenService{
 	public Orden save(Orden orden) {
 		// TODO Auto-generated method stub
 		return ordenRepository.save(orden);
+	}
+
+
+	@Override
+	public List<Orden> findAll() {
+		// TODO Auto-generated method stub
+		return ordenRepository.findAll();
+	}
+	
+	
+	//Metodo para generar numero de orden 
+	public String GenerarIdOrden() {
+		int numero = 0;
+		String numeroConcatenado = "";
+		
+		List<Orden> ordenes = findAll();
+		List<Integer> numeros = new ArrayList<>();
+		ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+		if (ordenes.isEmpty()) {
+			numero = 1;
+			
+		}else {
+			numero = numeros.stream().max(Integer::compareTo).get();
+			numero++;
+		}
+		
+		if (numero<10) {
+			
+			numeroConcatenado= "0000"+String.valueOf(numero);
+			
+		}else if (numero<100){
+			numeroConcatenado= "000"+String.valueOf(numero);
+		}
+		
+		
+		return numeroConcatenado;
 	}
 	
 	
